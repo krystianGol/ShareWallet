24bd6dbe4669503e0687e7bcf50514d9e98841bc
Krystian <krystianrafalgolda@gmail.com>
2024-03-22 21:03:20 +0100

    connect database

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 import express from "express"
+import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
 
+let users = [];
+let currentUserId = 1;
+
+const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "ShareWallet",
+    password: "db1234",
+    port: 5432
+});
+
+db.connect();
+
+db.query("SELECT * FROM users", (err, res) => {
+    if (err) {
+      console.error("Error executing query", err.stack);
+    } else {
+      users = res.rows;
+    }
+  });
+
 app.get("/", (req, res) => {
+    console.log(users);
     res.render("index.ejs");
 });
 
