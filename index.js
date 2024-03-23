b6e7af67db697141a519f85ae5ed8465e5065086
Krystian <krystianrafalgolda@gmail.com>
2024-03-23 13:54:43 +0100

    modified billingGroup index.js

--- index.js
+++ index.js
@@ -39,8 +39,33 @@ app.get("/", async (req, res) => {
         });
 });
 
-app.get("/billingGroup/:id", (req, res) => {
-    res.render("billingGroup.ejs");
+app.get("/billingGroup/:id", async (req, res) => {
+    const billingGroupId = req.params.id;
+
+    const itemsResult = await db.query("SELECT items.id, items.description, items.price, users.name, billing_group.title FROM items JOIN users ON users.id = items.user_id JOIN billing_group ON billing_group.id = items.billing_group_id WHERE billing_group_id = $1 ORDER BY items.id;", [billingGroupId]);
+
+    const usersResult = await db.query("SELECT users.name FROM users_billing_group JOIN users ON users.id = users_billing_group.user_id JOIN billing_group ON billing_group.id = users_billing_group.billing_group_id WHERE billing_group_id = $1;", [billingGroupId]);
+
+    const itemsData = itemsResult.rows;
+    console.log(itemsData);
+    const title = itemsData[0].title;
+
+    const usersData = usersResult.rows;
+    const users = usersData;
+
+    let allUsers = "";
+    users.forEach(user => {
+        allUsers += user.name + ", ";
+    });
+
+    let updatedAllUsers = allUsers.slice(0, -2);
+
+    res.render("billingGroup.ejs",
+        {
+            title: title,
+            users: updatedAllUsers,
+            items: itemsData
+        });
 });
 
 app.listen(port, () => {
